a909a6b2917694126fa854d99c0abfa1e5d6d6f1
Ahmed <elzek989@gmail.com>
2024-07-11 17:01:16 +0300

    Add SplashActivity

--- app/src/main/java/com/example/route/e_commerce/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/route/e_commerce/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.route.e_commerce
+package com.example.route.e_commerce.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.route.e_commerce.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/route/e_commerce/ui/splash/SplashActivity.kt
@@ -0,0 +1,28 @@
+package com.example.route.e_commerce.ui.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.route.e_commerce.R
+import com.example.route.e_commerce.ui.main.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        startMainActivity()
+    }
+
+    private fun startMainActivity() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            val mainIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
+            finish()
+        }, 2000)
+    }
+}
