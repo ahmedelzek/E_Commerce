f2ef5a9c824bb74af409b4ae583c689d80ddcf9c
Ahmed <elzek989@gmail.com>
2024-08-11 01:42:57 +0300

    Finish The Design For WishListFragment .

--- app/src/main/java/com/example/route/e_commerce/ui/main/fragments/wish_list/WishListFragment.kt
+++ app/src/main/java/com/example/route/e_commerce/ui/main/fragments/wish_list/WishListFragment.kt
@@ -1,4 +1,4 @@
-package com.example.route.e_commerce.ui.main.fragments.wishList
+package com.example.route.e_commerce.ui.main.fragments.wish_list
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
